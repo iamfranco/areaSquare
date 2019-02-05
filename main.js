0f334673cfecc4ec6c8b04d41cf0b8ffc330eeef
Franco <kinfaifrancochan@gmail.com>
2019-02-05 06:58:42 +0000

    add input highlighting

--- main.js
+++ main.js
@@ -7,15 +7,43 @@ var equals_lengths = $('.equals_lengths');
 
 var area = 0;
 var px500tomm = 0;
+update_defect_box_area();
 
 input_area.addEventListener('keyup', function(event) {
   area = parseInt(this.value)
+  if (this.value.length > 0) {
+    this.classList.remove('totype');
+  } else {
+    area = 0;
+    if (input_measured.value.length > 0) {
+      this.classList.add('totype');
+    }
+  }
   update_defect_box_area();
 })
 
 input_measured.addEventListener('keyup', function(event) {
+  if (this.value.length > 0) {
+    this.classList.remove('totype');
+    if (input_area.value.length > 0) {
+      input_area.classList.remove('totype');
+    } else {
+      input_area.classList.add('totype');
+    }
+  } else {
+    this.classList.add('totype');
+    input_area.classList.remove('totype');
+  }
+
   px500tomm = parseInt(this.value);
   update_defect_box_area();
+})
+
+input_measured.addEventListener('keydown', function(event) {
+  if (event.keyCode == 9) { // Tab key
+    event.preventDefault();
+    input_area.focus();
+  }
 })
 
 function update_defect_box_area() {
